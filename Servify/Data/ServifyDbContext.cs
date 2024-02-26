105a03715a9cc79732ce04187e94f2c016a48784
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2024-02-26 18:31:48 +0400

    add nuget packages of entityframework and make the connection

---
+++ Servify/Data/ServifyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Servify.Models;
+
+namespace Servify.Data
+{
+    public class ServifyDbContext: DbContext
+    {
+        public ServifyDbContext(DbContextOptions<ServifyDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Models.ServiceProvider> ServiceProviders { get; set; }
+    }
+}

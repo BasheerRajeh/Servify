6f50aaf364715dee96a1f4531e3e2d2564003306
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2024-02-26 21:32:29 +0400

    add dtos for models

---
+++ Servify/DTOs/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Servify.DTOs
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Position { get; set; } = String.Empty;
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Servify/DTOs/RestaurantDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Servify.DTOs
+{
+    public class RestaurantDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Location { get; set; } = String.Empty;
+        public List<EmployeeDto> Employees { get; set; } = new List<EmployeeDto>();
+    }
+}
